2d836d6270d6531fcb70ed342097542051f398bf
Ander981 <andersonmichel981@gmail.com>
2026-02-19 11:18:08 -0300

    Correction: dtabase

--- config/database.php
+++ config/database.php
@@ -1,10 +1,21 @@
 <?php
 // config/database.php
 
-$host = 'localhost';
-$dbname = 'bibliotheque';
-$username = 'root';       // par défaut 
-$password = '';           // par défaut
+// Détection de l'environnement
+if (isset($_ENV['VERCEL'])) {
+    // Nous sommes sur Vercel, on utilise les variables d'env.
+  $host = isset($_ENV['DB_HOST']) ? $_ENV['DB_HOST'] : getenv('DB_HOST');
+   $dbname = isset($_ENV['DB_NAME']) ? $_ENV['DB_NAME'] : getenv('DB_NAME');
+$user   = isset($_ENV['DB_USER']) ? $_ENV['DB_USER'] : getenv('DB_USER');
+$pass   = isset($_ENV['DB_PASSWORD']) ? $_ENV['DB_PASSWORD'] : getenv('DB_PASSWORD');
+
+} else {
+    // Nous sommes en local (WAMP/XAMPP)
+    $host = 'localhost';
+    $dbname = 'bibliotheque';
+    $username = 'root';
+    $password = '';
+}
 
 try {
     $pdo = new PDO("mysql:host=$host;dbname=$dbname;charset=utf8", $username, $password);
